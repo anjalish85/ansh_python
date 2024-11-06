a449d5183cf25ccd32c5b5776b67cd542d431f33
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2024-11-06 20:11:13 +0530

    Time: 167 ms (14.02%) | Memory: 148.6 MB (10.69%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    // Helper function to check if a string is a palindrome
-    bool isPalindrome(const string &s, int start, int end) {
-        while (start < end) {
-            if (s[start] != s[end]) {
+    bool pal(string s, int st, int end){
+        while (st<end){
+            if (s[st]!=s[end]){
                 return false;
             }
-            start++;
+            st++;
             end--;
         }
         return true;
     }
-
-    // Recursive function to generate the partitions
-    void solve(string &s, int idx, vector<string> &temp, vector<vector<string>> &ans) {
-        // Base case: if we've reached the end of the string, add the current partition
-        if (idx == s.size()) {
-            ans.push_back(temp);
+    
+    void solve(vector <vector<string>> &ds, vector <string> t, string s, int idx){
+        if (idx==s.size()){
+            ds.push_back(t);
             return;
         }
-        
-        // Try all possible substrings starting from idx
-        for (int i = idx; i < s.size(); i++) {
-            if (isPalindrome(s, idx, i)) {
-                // Add palindrome substring to current partition
-                temp.push_back(s.substr(idx, i - idx + 1));
-                // Recurse with the next substring
-                solve(s, i + 1, temp, ans);
-                // Backtrack and remove the last added substring
-                temp.pop_back();
+        for (int i=idx; i<s.size(); i++){
+            if (pal(s, idx, i)){
+                t.push_back(s.substr(idx, i-idx+1));
+                solve(ds, t, s, i+1);
+                t.pop_back();
             }
-        }
+        }       
     }
-
+    
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> temp;
-        solve(s, 0, temp, ans);
-        return ans;
+        
+        vector <vector<string>> ds;
+        vector <string> t;
+        
+        solve(ds, t, s, 0);
+        
+        return ds;
     }
 };
