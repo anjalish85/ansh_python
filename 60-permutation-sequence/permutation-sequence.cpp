9c1c217ca159e9bc2ffd296e9daf66c3c96bdc30
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2024-11-08 11:59:48 +0530

    Time: 48 ms (16.23%) | Memory: 8.6 MB (16.61%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     
-    int fn(int n){
-        if (n==0 || n==1){
-            return 1;
-        }
-        return n*fn(n-1);
-    }
-    
-    void valueadd(int k, int n, vector <int>  arr, string &s){
-        if (n==0) return;
-        
-        int idx=(k-1)/fn(n-1);
-        
-        s+=to_string(arr[idx]);
-        arr.erase(arr.begin()+idx);
-        k=k-idx*fn(n-1);
-        
-        valueadd(k, n - 1, arr, s); 
-        
-    }
     string getPermutation(int n, int k) {
         
         vector <int> num;
-        for(int i=1; i<=n; i++){
+        for (int i=1; i<=n; i++){
             num.push_back(i);
         }
-        string t;
-        
-        valueadd(k, n , num, t);
-        return t;
+        string s="";
+        for (int i=0; i<n; i++){
+            s+=to_string(num[i]);
+        }
+
+        for (int i=1; i<k; i++){
+            next_permutation(s.begin(), s.end());
+        }
+
+        return s;
     }
+
 };
