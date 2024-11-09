746512c8fad7da04c8eea2f899306507619d3dc1
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2024-11-09 10:33:10 +0530

    Time: 5 ms (17.55%) | Memory: 16.1 MB (9.89%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-
-void solve(int idx, vector <int> nums, vector <int> temp, vector<vector<int>> &ds){
+void solve(int idx,  vector <int> nums, vector < vector <int>> &ds,  vector <int> temp){
     if (idx==nums.size()){
-        ds.push_back(temp); return;
+        ds.push_back(temp);
+        return;
     }
-
-    temp.push_back(nums[idx]);  
-    solve(idx+1, nums, temp, ds);  
+    
+    temp.push_back(nums[idx]);
+    solve(idx+1, nums, ds, temp );
     temp.pop_back();
-
-    while(idx+1<nums.size() && nums[idx]==nums[idx+1]){
+    while (idx+1<nums.size() && nums[idx]==nums[idx+1]){
         idx++;
     }
-    solve(idx+1, nums, temp, ds);    
+    solve(idx+1, nums, ds, temp );
 }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-
-        sort(nums.begin(), nums.end());
+        
+        vector < vector <int>> ds;
         vector <int> temp;
-        vector<vector<int>> ds;
+        sort(nums.begin(), nums.end());
 
-        solve(0, nums, temp, ds);
-
+        solve (0, nums, ds, temp);
         return ds;
     }
 };
