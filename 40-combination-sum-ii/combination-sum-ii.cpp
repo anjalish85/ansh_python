07235fc17d3b86b1cf74c1d86d0b6e0dc9fee400
Anjali Shaw <65438703+anjalish85@users.noreply.github.com>
2024-11-09 10:51:21 +0530

    Time: 113 ms (5.10%) | Memory: 79.9 MB (5.00%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    void solve(int idx, vector<int>& arr, int target, vector<vector<int>>& ds, vector<int>& temp) {
-            if (target == 0) {
-                ds.push_back(temp);return;
-            }
-        
-            for (int i=idx; i<arr.size(); i++){
-                if (i > idx && arr[i]==arr[i-1]) continue;
-                if (arr[i] > target) break;
-            
-                temp.push_back(arr[i]);
-                solve(i+1, arr, target - arr[i], ds, temp); // Pass `idx` again to
-                temp.pop_back(); 
-            }
-        
-        //solve(idx+1, arr, target , ds, temp);
-        
+void solve(int idx, vector<int> nums, int target, vector <vector<int>>& ds, vector<int> t){
+    if (idx==nums.size()){
+        if (target==0){
+            ds.push_back(t);
+        }
+        return;
     }
 
+    if (nums[idx]<=target){
+        t.push_back(nums[idx]);
+        solve(idx+1, nums, target-nums[idx], ds, t);
+        t.pop_back();
+    }
+
+    while(idx+1<nums.size() && nums[idx]==nums[idx+1]){
+        idx++;
+    }
+
+    solve(idx+1, nums, target, ds, t);   
+}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ds;
-        vector<int> temp;
-        
+        vector<vector<int>> ds; 
+        vector <int> t;
+
         sort(candidates.begin(), candidates.end());
-        solve(0, candidates, target, ds, temp);
+        solve(0, candidates, target, ds, t);
+
         return ds;
     }
 };
